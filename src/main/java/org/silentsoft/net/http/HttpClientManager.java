51621e7f2abb198d979cec894704f2e64fbb8b17
silentsoft <hyesung.lee@silentsoft.org>
2016-04-15 16:33:19 +0900

    moved http and rest class from context project. moved item and pojo class from silentsoft-core prject.

---
+++ src/main/java/org/silentsoft/net/http/HttpClientManager.java
@@ -0,0 +1,154 @@
+package org.silentsoft.net.http;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.silentsoft.net.item.StoreItem;
+import org.silentsoft.net.pojo.FilePOJO;
+import org.slf4j.helpers.MessageFormatter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HttpClientManager {
+	
+	static {
+		/**
+		 * WARNING : DO NOT REMOVE BELOW CODE ! THIS IS VERY IMPORTANT FOR PERFORMANCE !!!
+		 */
+		Logger.getLogger("org.apache.http").setLevel(Level.OFF);
+	}
+	
+	private static enum RequestType {
+		GET,
+		POST,
+		MULTIPART
+	};
+
+	public static <T> T doGet(String uri, Class<T> returnType) throws Exception {
+		return doGet(uri, null, returnType);
+	}
+	
+	public static <T> T doGet(String uri, Header[] headers, Class<T> returnType) throws Exception {
+		return doAction(uri, null, null, returnType, RequestType.GET);
+	}
+	
+	public static <T> T doPost(String uri, Object param, Class<T> returnType) throws Exception {
+		return doPost(uri, null, param, returnType);
+	}
+	
+	public static <T> T doPost(String uri, Header[] headers, Object param, Class<T> returnType) throws Exception {
+		return doAction(uri, headers, param, returnType, RequestType.POST);
+	}
+	
+	public static <T> T doMultipart(String uri, Object param, Class<T> returnType) throws Exception {
+		return doMultipart(uri, null, param, returnType);
+	}
+	
+	public static <T> T doMultipart(String uri, Header[] headers, Object param, Class<T> returnType) throws Exception {
+		return doAction(uri, headers, param, returnType, RequestType.MULTIPART);
+	}
+	
+	private static <T> T doAction(String uri, Header[] headers, Object param, Class<T> returnType, RequestType requestType) throws Exception {
+		T returnValue = null;
+		
+		HttpGet httpGet = null;
+		HttpPost httpPost = null;
+		HttpEntity httpEntity = null;
+		CloseableHttpResponse httpResponse = null;
+		
+		try {
+			switch (requestType) {
+				case GET:
+				{
+					httpGet = new HttpGet(uri);
+					
+					httpResponse = execute(httpGet, headers);
+					break;
+				}
+				case POST:
+				{
+					httpPost = new HttpPost(uri);
+					
+					if (param != null) {
+						StringEntity stringEntity = new StringEntity(new JSONObject(param).toString(), Charset.forName("UTF-8"));
+						stringEntity.setContentType("application/json; charset=UTF-8");
+						
+						httpPost.setEntity(stringEntity);
+					}
+					
+					httpResponse = execute(httpPost, headers);
+					break;
+				}
+				case MULTIPART:
+				{
+					MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create().setCharset(Charset.forName("UTF-8"));
+					
+					if (param instanceof StoreItem) {
+						StoreItem storeItem = (StoreItem) param;
+						
+						for (FilePOJO filePOJO : storeItem) {
+							if (filePOJO.getFile() != null) {
+								multipartEntityBuilder.addBinaryBody("binary", filePOJO.getFile(), ContentType.APPLICATION_OCTET_STREAM, filePOJO.getNameWithExtension());
+							}
+							
+							filePOJO.setFile(null);
+							filePOJO.setBytes(null);
+						}
+						
+						multipartEntityBuilder.addTextBody("json", new JSONObject(storeItem).toString(), ContentType.APPLICATION_JSON);
+					} else {
+						return null;
+					}
+					
+					httpPost = new HttpPost(uri);
+					httpPost.setEntity(multipartEntityBuilder.build());
+					
+					httpResponse = execute(httpPost, headers);
+					break;
+				}
+			}
+			
+			httpEntity = httpResponse.getEntity();
+			if (httpEntity != null) {
+				String body = EntityUtils.toString(httpEntity, Charset.forName("UTF-8"));
+				if (body.length() > 0) {
+					returnValue = (T)new ObjectMapper().readValue(body, returnType);
+				}
+			}
+			
+			EntityUtils.consume(httpEntity);
+		} catch (Exception e) {
+			throw new Exception(MessageFormatter.arrayFormat("Failed connection to <{}> !", new Object[]{uri}).getMessage(), e);
+		} finally {			
+			if (httpResponse != null) {
+				try {
+					httpResponse.close();
+				} catch (IOException ioe) {
+					throw new Exception("Failed close to http response !", ioe);
+				}
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	private static CloseableHttpResponse execute(HttpUriRequest request, Header[] headers) throws IOException, ClientProtocolException {
+		request.setHeaders(headers);
+		
+		return HttpClientFactory.getHttpClient().execute(request);
+	}
+}
